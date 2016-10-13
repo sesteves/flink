0fb319952b5b628b6b1cfd725f9c32b1b1491244
sesteves <sroesteves@gmail.com>
2016-10-13 22:42:52 +0100

    imports optimized

--- flink-runtime/src/main/java/org/apache/flink/runtime/state/hybrid/BucketList.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/state/hybrid/BucketList.java
@@ -23,8 +23,6 @@ import flexjson.JSONSerializer;
 import scala.Tuple2;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
